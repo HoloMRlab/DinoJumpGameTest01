2a3cf4e18f8e07fdd0c1305bbb51b2fa308610c3
HoloMRlab <60426016+HoloMRlab@users.noreply.github.com>
2020-01-29 18:35:46 +0900

    Add files via upload

---
+++ Assets/Scripts/CactusPool.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CactusPool : MonoBehaviour
+{
+    //Global variables
+    //선인장 갯수=poolSize
+    public int poolSize = 1;
+
+    //obstaclePrefab라는 프리팹형태 정보 저장
+    public GameObject obstaclePrefab;
+
+    //????
+    public float spawnRate = 4.0f;
+
+    //최소 위치
+    public float cactusMin = 3.0f;
+
+    //최대 위치
+    public float cactusMax = 5.0f;
+
+    //Local variables
+    //Gameobject 배열, cactusObstacles라는 배열 선언
+    private GameObject[] cactusObstacles;
+
+    //선인장 시작점
+    private Vector2 poolPosition = new Vector2(3.5f, 0.0f);
+
+    //마지막에 나타난 선인장 시간
+    private float timeSinceLastSpawned;
+
+    private int currentCactus = 0;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        cactusObstacles = new GameObject[poolSize];
+        for (int i = 0; i < poolSize; i++)
+        {
+            cactusObstacles[i] = (GameObject)Instantiate(obstaclePrefab, poolPosition, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        timeSinceLastSpawned += Time.deltaTime;
+
+        if (GameControl.instance.gameOver == false && timeSinceLastSpawned >= spawnRate)
+        {
+            timeSinceLastSpawned = 0;
+            float spawnXposition = Random.Range(cactusMin, cactusMax);
+            cactusObstacles[currentCactus].transform.position = new Vector2(spawnXposition, 0.0f);
+
+            currentCactus++;
+            if (currentCactus >= poolSize)
+            {
+                currentCactus = 0;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/CactusRandom.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CactusRandom : MonoBehaviour
+{
+    public GameObject cactusPrefab;
+    public float minX = 0;
+    public float maxX = 0;
+
+    private float spawnRate;
+    private GameObject cactusSpawn;
+    private Vector2 startPosition = new Vector2(3.5f, 0.0f);
+    private float timeLastSpawned;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        cactusSpawn = (GameObject)Instantiate(cactusPrefab, startPosition, Quaternion.identity);
+        spawnRate = 2.0f;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        timeLastSpawned += Time.deltaTime;
+        if (GameControl.instance.gameOver == false && timeLastSpawned >= spawnRate)
+        {
+            timeLastSpawned = 0;
+            float randomRange = Random.Range(startPosition.x + minX, startPosition.x + maxX);
+            cactusSpawn.transform.position = new Vector2(randomRange, 0.0f);
+        }
+    }
+}
